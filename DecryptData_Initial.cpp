bfd136c72903606eb99e042d7c8bc3ffbc11a257
dmart100 <anddagger@gmail.com>
2020-04-22 21:19:02 -0500

    added more comments in asm format for readability. push/pop the eax,ebx,ecx registers so that our steps dont interfere with the indexes and loop counter.

--- DecryptData_Initial.cpp
+++ DecryptData_Initial.cpp
@@ -23,36 +23,98 @@ int decryptData(char *data, int dataLength)
 
 		// you will need to reference some of these global variables
 		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
-		
-		mov esi, gptrPasswordHash // put the address of gPasswordHash into %esi
-		xor eax, eax //
-		mov al, byte ptr[esi] // store gPassword[0] in %al
-		shl ax,8 // left shift by 8 (multiply by 256)
-		xor ecx,ecx // set %ecx = 0
-		mov cl, byte ptr[esi + 1] // set %cx to gPassword[1]
-		add ax, cx 	// add gPassword[1] to %ax, %ax is now the starting index for key.dat
-					// ax = starting_index = gPasswordHash[0] * 256 + gPasswordHash[1]
+			mov esi, gptrPasswordHash;	put the address of gPasswordHash into esi
+			xor eax, eax
+			mov al, byte ptr[esi];		store gPassword[0] in %al
+			shl ax, 8;					left shift by 8 (multiply by 256)
+			xor ecx, ecx;				set ecx = 0
+			mov cl, byte ptr[esi + 1];	set cx to gPassword[1]
+			add ax, cx;					add gPassword[1] to ax, ax is now the starting index for the keyFile
+			// ax = starting_index = gPasswordHash[0] * 256 + gPasswordHash[1]
 
-		xor ebx, ebx // %ebx = control variable (loop)
-		xor ecx,ecx
-		mov ecx, dataLength // %ecx = length
-		cmp ecx, 0 // check that the length is not less than or equal to 0
-		sub ecx, 1 // ecx-- (file length is 1 less than previous)
-		jbe lbl_EXIT_ZERO_LENGTH //
+			xor ebx, ebx;				ebx = control variable(loop)
+			xor ecx, ecx
+			mov ecx, dataLength;		ecx = length
+			cmp ecx, 0;					check that the length is not less than or equal to 0
+			sub ecx, 1;					ecx-- (file length is 1 less than previous)
+			jbe lbl_EXIT_ZERO_LENGTH
 
-		mov edi, data // put the address of first byte into %edi
-		mov esi, gptrKey // put the address of gKey into %esi
+			mov edi, data;				put the address of first byte of data in %edi
+			mov esi, gptrKey;			put the address of gKey into %esi
+	
+			//
+			// LOOP THROUGH ENTIRE data[] BYTE BY BYTE
+			// At each step, data is load from the dl register and that step is performed on the data
+			// The modified data is then loaded back into dl
+		lbl_LOOP:
+				mov dl, byte ptr[edi + ebx];	get the current byte being manipulated
+				
+				// Free up the registers for use in Steps A - E
+				push eax
+				push ebx
+				push ecx
+				
+				// Part A reverse bit order	- value will be in 'dl'
+				mov al, dl;		load data from previous step into al
+				mov cl, 7;		starting byte position to shift maninpulated data into
+				mov dh, 1;		dh is a 1 which travels in the byte, moving 1 position from right to left with each iteration
+				mov dl, 0;		clear dl for use as loop counter
 
-	/* LOOP THROUGH ENTIRE data[] -- BYTE BY BYTE */
-		lbl_LOOP :
-		mov dl, byte ptr[edi + ebx] //
-		xor dl, byte ptr[esi + eax] // data[ebx] = data[ebx] xor with keyfile[starting_index]
-		mov byte ptr[edi + ebx], dl //
+		LOOP1 : push ax
+				AND al, dh;		first time through loop AND with 0000 0000b, second time with 0000 0001b, third time with 0000 0010b...
+				push cx;		save previous value of cx
+				mov cl, dl;		load the value of loop counter into cl
+				shr al, cl;		shr by value of loop counter(first time by 0, second time by 1...)
+				pop cx;			restore cx
+				mov bh, al;		load the shifted result into bh
+				shl bh, cl;		shift into position - '7' for byte 0, '6' for byte 1...
+				OR ch, bh;		OR to the final result
+				DEC cl;			decrease byte position to shift manipulated data into
+				INC dl;			raise the loop counter by 1
+				shl dh, 1;		first time through loop dh = 0000 0001b, second time dh = 0000 0010b...
+				pop ax;
+				cmp dl, 8;		when loop counter reaches 8, exit to END
+				je END
+				jmp LOOP1
+		END :
+			xor dh, dh;		clear dh so that when we loop back to Step E, edx = dl
+			mov dl, ch;		save modified data in dl
+			//*/
 
-		add ebx, 1 // increment %ebx by 1
-		cmp ebx, ecx // if(ebx > ecx) { end loop }
-		ja lbl_EXIT_END //
-		jmp lbl_LOOP // else { loop }
+			// Part C swap half nibbles
+			mov eax, edx;		load data to be swapped in eax
+			lea bl, [eax * 4];	shift data to the left 2 and save in bl
+			and bl, 0xCC;		masking to get indexes we want to swap eg. 1100 1100
+			shr al, 2;			shift original data 2 to the right
+			and al, 0x33;		mask remaining indexes eg. 0011 0011
+			or dl, bl;			combine and save in dl
+			//*/
+
+			// Part E, swapping dl with the table value.
+			mov dl, gEncodeTable[edx]
+			//*/
+
+			// Part B invert bits 0, 3, 6
+			xor	dl, 0x49; bitMask = 0x49
+			//*/
+
+			// Part D rotate 3 bits right
+			ror dl, 3
+			//*/
+
+			// Restore registers back to their previous states and save the modified data
+			pop eax
+			pop ebx
+			pop ecx
+
+			// XOR with the keyFile
+			xor dl, byte ptr[esi + eax];	data[ebx] = data[ebx] xor with keyfile[starting_index]
+
+			// LOOP control
+			add ebx, 1;						increment loop counter by 1
+			cmp ebx, ecx;					if dataLength of the input file > loop counter, exit to lbl_EXIT
+			ja lbl_EXIT_END
+			jmp lbl_LOOP;					if dataLength < loop counter, jump back to lbl_LOOP
 
 		lbl_EXIT_ZERO_LENGTH :
 		sub ebx, 1 // decrement ebx to -1 to return failure
--- EncryptData_Initial.cpp
+++ EncryptData_Initial.cpp
@@ -20,78 +20,140 @@ int encryptData(char *data, int dataLength)
 	// Also, you cannot use a lot of global variables - work with registers
 
 	__asm {
-		
+
 		// you will need to reference some of these global variables
 		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
 
-		mov esi, gptrPasswordHash // put the address of gPasswordHash into %esi
-		xor eax, eax //
-		mov al, byte ptr[esi] // store gPassword[0] in %al
-		shl ax,8 // left shift by 8 (multiply by 256)
-		xor ecx,ecx // set %ecx = 0
-		mov cl, byte ptr[esi + 1] // set %cx to gPassword[1]
-		add ax, cx // add gPassword[1] to ax, ax is now the starting index for key.dat
-					// ax = starting_index = gPasswordHash[0] * 256 + gPasswordHash[1]
+			mov esi, gptrPasswordHash;	put the address of gPasswordHash into esi
+			xor eax, eax
+			mov al, byte ptr[esi];		store gPassword[0] in %al
+			shl ax, 8;					left shift by 8 (multiply by 256)
+			xor ecx, ecx;				set ecx = 0
+			mov cl, byte ptr[esi + 1];	set cx to gPassword[1]
+			add ax, cx;					add gPassword[1] to ax, ax is now the starting index for the keyFile
+			// ax = starting_index = gPasswordHash[0] * 256 + gPasswordHash[1]
 
-		xor ebx, ebx // %ebx = control variable (loop)
-		xor ecx,ecx
-		mov ecx, dataLength // %ecx = length
-		cmp ecx, 0 // check that the length is not less than or equal to 0
-		sub ecx, 1 // ecx-- (file length is 1 less than previous)
-		jbe lbl_EXIT_ZERO_LENGTH //
+			xor ebx, ebx;			ebx = control variable(loop)
+			xor ecx, ecx
+			mov ecx, dataLength;	ecx = length
+			cmp ecx, 0;				check that the length is not less than or equal to 0
+			sub ecx, 1;				ecx-- (file length is 1 less than previous)
+			jbe lbl_EXIT_ZERO_LENGTH
 
-		mov edi, data // put the address of first byte of data in %edi
-		mov esi, gptrKey // put the address of gKey into %esi
+			mov edi, data;		put the address of first byte of data in %edi
+			mov esi, gptrKey;	put the address of gKey into %esi
 
-	//
-	// LOOP THROUGH ENTIRE data[] BYTE BY BYTE
-	//
+			//
+			// LOOP THROUGH ENTIRE data[] BYTE BY BYTE
+			// At each step, data is load from the dl register and that step is performed on the data
+			// The modified data is then loaded back into dl
 		lbl_LOOP :
-		mov dl, byte ptr[edi + ebx] //
-		xor dl, byte ptr[esi + eax] // data[ebx] = data[ebx] xor with keyfile[starting_index]
-		mov byte ptr[edi + ebx], dl //
+				mov dl, byte ptr[edi + ebx];	get the current byte being manipulated
 
-		add ebx, 1 // increment %ebx by 1
-		cmp ebx, ecx // if(ebx > ecx) { end loop }
-		ja lbl_EXIT_END //
-		jmp lbl_LOOP // else { loop }
+				// XOR with the keyFile
+				xor dl, byte ptr[esi + eax];	data[ebx] = data[ebx] xor with keyfile[starting_index]
+
+				// Free up the registers for use in Steps A - E 
+				push eax
+				push ebx
+				push ecx				
+				
+				// Part D rotate 3 bits right
+				ror dl, 3
+				//*/
+
+				// Part B invert bits 0, 3, 6
+				xor	dl, 0x49; bitMask = 0x49
+				//*/
+
+				// Part E, swapping dl with the table value.
+				mov dl, gEncodeTable[edx]
+				//*/
+
+				// Part C swap half nibbles
+				mov eax, edx;		load data to be swapped in eax
+				lea bl, [eax * 4];	shift data to the left 2 and save in bl
+				and bl, 0xCC;		masking to get indexes we want to swap eg. 1100 1100
+				shr al, 2;			shift original data 2 to the right
+				and al, 0x33;		mask remaining indexes eg. 0011 0011
+				or dl, bl;			combine and save in dl
+				//*/
+
+				// Part A reverse bit order	- value will be in 'dl'
+				mov al, dl;		load data from previous step into al
+				mov cl, 7;		starting byte position to shift maninpulated data into
+				mov dh, 1;		dh is a 1 which travels in the byte, moving 1 position from right to left with each iteration
+				mov dl, 0;		clear dl for use as loop counter
+
+		LOOP1 : push ax
+				AND al, dh;		first time through loop AND with 0000 0000b, second time with 0000 0001b, third time with 0000 0010b...
+				push cx;		save previous value of cx
+				mov cl, dl;		load the value of loop counter into cl
+				shr al, cl;		shr by value of loop counter(first time by 0, second time by 1...)
+				pop cx;			restore cx
+				mov bh, al;		load the shifted result into bh
+				shl bh, cl;		shift into position - '7' for byte 0, '6' for byte 1...
+				OR ch, bh;		OR to the final result
+				DEC cl;			decrease byte position to shift manipulated data into
+				INC dl;			raise the loop counter by 1
+				shl dh, 1;		first time through loop dh = 0000 0001b, second time dh = 0000 0010b...
+				pop ax;			
+				cmp dl, 8;		when loop counter reaches 8, exit to END
+				je END
+				jmp LOOP1
+		   END :
+				xor dh, dh;		clear dh so that when we loop back to Step E, edx = dl
+				mov dl, ch;		save modified data in dl
+				//*/
+
+				// Restore registers back to their previous states and save the modified data
+				pop eax
+				pop ebx
+				pop ecx
+				
+				// LOOP control
+				add eax, 1;			increment keyFile index by 1
+				add ebx, 1;			increment loop counter by 1
+				cmp ebx, ecx;		if dataLength of the input file > loop counter, exit to lbl_EXIT
+				ja lbl_EXIT_END
+				jmp lbl_LOOP;		if dataLength < loop counter, jump back to lbl_LOOP
 
 		lbl_EXIT_ZERO_LENGTH :
-		sub ebx, 1 // decrement ebx to -1 to return failure
-		jmp lbl_EXIT //
+			sub ebx, 1;		decrement ebx to - 1 to return failure
+			jmp lbl_EXIT
 
 		lbl_EXIT_END :
-		xor ebx, ebx // ebx = 0, correctly executed
+			xor ebx, ebx;	ebx = 0, correctly executed
 
 		lbl_EXIT :
-		mov resulti, ebx
-		
-		
-		/*// simple example that xors 2nd byte of data with 14th byte in the key file
-		lea esi,gkey				// put the ADDRESS of gkey into esi
-		mov esi,gptrKey;			// put the ADDRESS of gkey into esi (since *gptrKey = gkey)
+			mov resulti, ebx
 
-		lea	esi,gPasswordHash		// put ADDRESS of gPasswordHash into esi
-		mov esi,gptrPasswordHash	// put ADDRESS of gPasswordHash into esi (since unsigned char *gptrPasswordHash = gPasswordHash)
 
-		mov al,byte ptr [esi]				// get first byte of password hash
-		mov al,byte ptr [esi+4]				// get 5th byte of password hash
-		mov ebx,2
-		mov al,byte ptr [esi+ebx]			// get 3rd byte of password hash
-		mov al,byte ptr [esi+ebx*2]			// get 5th byte of password hash
+			/*// simple example that xors 2nd byte of data with 14th byte in the key file
+			lea esi,gkey				// put the ADDRESS of gkey into esi
+			mov esi,gptrKey;			// put the ADDRESS of gkey into esi (since *gptrKey = gkey)
 
-		mov ax,word ptr [esi+ebx*2]			// gets 5th and 6th bytes of password hash ( gPasswordHash[4] and gPasswordHash[5] ) into ax
-		mov eax,dword ptr [esi+ebx*2]		// gets 4 bytes, as in:  unsigned int X = *( (unsigned int*) &gPasswordHash[4] );
+			lea	esi,gPasswordHash		// put ADDRESS of gPasswordHash into esi
+			mov esi,gptrPasswordHash	// put ADDRESS of gPasswordHash into esi (since unsigned char *gptrPasswordHash = gPasswordHash)
 
-		mov al,byte ptr [gkey+ebx]			// get's 3rd byte of gkey[] data
+			mov al,byte ptr [esi]				// get first byte of password hash
+			mov al,byte ptr [esi+4]				// get 5th byte of password hash
+			mov ebx,2
+			mov al,byte ptr [esi+ebx]			// get 3rd byte of password hash
+			mov al,byte ptr [esi+ebx*2]			// get 5th byte of password hash
 
-		mov al,byte ptr [gptrKey+ebx]		// THIS IS INCORRECT - will add the address of the gptrKey global variable (NOT the value that gptrKey holds)
+			mov ax,word ptr [esi+ebx*2]			// gets 5th and 6th bytes of password hash ( gPasswordHash[4] and gPasswordHash[5] ) into ax
+			mov eax,dword ptr [esi+ebx*2]		// gets 4 bytes, as in:  unsigned int X = *( (unsigned int*) &gPasswordHash[4] );
 
-		mov al,byte ptr [esi+0xd];			// access 14th byte in gkey[]: 0, 1, 2 ... d is the 14th byte
-		mov edi,data				// Put ADDRESS of first data element into edi
-		xor byte ptr [edi+1],al		// Exclusive-or the 2nd byte of data with the 14th element of the keyfile
-									// NOTE: Keyfile[14] = 0x21, that value changes the case of a letter and flips the LSB
-									// Capital "B" = 0x42 becomes lowercase "c" since 0x42 xor 0x21 = 0x63	*/
+			mov al,byte ptr [gkey+ebx]			// get's 3rd byte of gkey[] data
+
+			mov al,byte ptr [gptrKey+ebx]		// THIS IS INCORRECT - will add the address of the gptrKey global variable (NOT the value that gptrKey holds)
+
+			mov al,byte ptr [esi+0xd];			// access 14th byte in gkey[]: 0, 1, 2 ... d is the 14th byte
+			mov edi,data				// Put ADDRESS of first data element into edi
+			xor byte ptr [edi+1],al		// Exclusive-or the 2nd byte of data with the 14th element of the keyfile
+			// NOTE: Keyfile[14] = 0x21, that value changes the case of a letter and flips the LSB
+			// Capital "B" = 0x42 becomes lowercase "c" since 0x42 xor 0x21 = 0x63	*/
 	}
 
 	return resulti;
